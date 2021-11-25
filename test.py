fa3208df794839e540fb469e17a2bc0112738fe4
Dawid Dęby <ddeby@totalbet.pl>
2021-11-25 16:10:58 +0100

    gitignore and add database

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(f"{BASE}video/1", {"likes": 10, "name": "Name", "views": 10})
+response = requests.put(f"{BASE}video/1112", {"likes": 10, "name": "Name", "views": 10})
 print(response.json())
-response = requests.put(f"{BASE}video/1", {"likes": 10, "name": "Name", "views": 10})
-print(response.json())
+# response = requests.put(f"{BASE}video/1", {"likes": 10, "name": "Name", "views": 10})
+# print(response.json())
 
 
-response = requests.delete(f"{BASE}video/1")
-print(response)
+# response = requests.delete(f"{BASE}video/1")
+# print(response)
 
 response = requests.get(f"{BASE}video/2")
 print(response.json())

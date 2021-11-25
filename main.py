3eec3c506d47c83eac2939964b7a3f50764e888a
Dawid Dęby <ddeby@totalbet.pl>
2021-11-25 15:55:06 +0100

    delete

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument(
     "name", type=str, help="Name of the video is required", required=True
@@ -24,16 +25,27 @@ def abort_if_video_id_doesnt_exist(video_id):
         abort(404, "Video id is not valid ...")
 
 
+def abort_if_video_id_exist(video_id):
+    if video_id in videos:
+        abort(409, "Video already exist...")
+
+
 class Video(Resource):
     def get(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_id_exist(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         print(videos)
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return "", 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -4,7 +4,12 @@ BASE = "http://127.0.0.1:5000/"
 
 response = requests.put(f"{BASE}video/1", {"likes": 10, "name": "Name", "views": 10})
 print(response.json())
+response = requests.put(f"{BASE}video/1", {"likes": 10, "name": "Name", "views": 10})
+print(response.json())
 
+
+response = requests.delete(f"{BASE}video/1")
+print(response)
 
 response = requests.get(f"{BASE}video/2")
 print(response.json())
